4717b7f576fbb332f2b785b83a4802677150d182
Greg Lucas <greg.m.lucas@gmail.com>
2025-07-17 09:09:50 -0600

    MNT: Remove api_key variables from upload

--- imap_data_access/io.py
+++ imap_data_access/io.py
@@ -387,16 +387,13 @@ def reprocess(
         logger.debug("Received JSON: %s", items)
 
 
-def upload(file_path: Union[Path, str], *, api_key: Optional[str] = None) -> None:
+def upload(file_path: Union[Path, str]) -> None:
     """Upload a file to the data archive.
 
     Parameters
     ----------
     file_path : pathlib.Path or str
         Path to the file to upload.
-    api_key : str, optional
-        API key to authenticate with the data access API. If not provided,
-        the value from the IMAP_API_KEY environment variable will be used.
     """
     file_path = Path(file_path).resolve()
     if not file_path.exists():
@@ -405,9 +402,6 @@ def upload(file_path: Union[Path, str], *, api_key: Optional[str] = None) -> Non
     # The upload name needs to be given as a path parameter
     url = f"{imap_data_access.config['DATA_ACCESS_URL']}/upload/{file_path.name}"
     logger.info("Uploading file %s to %s", file_path, url)
-
-    # Create a request header with the API key
-    api_key = api_key or imap_data_access.config["API_KEY"]
 
     # We send a GET request with the filename and the server
     # will respond with an s3 presigned URL that we can use
