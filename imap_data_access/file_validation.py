2fc6d0af042eb2f0069346c63a71b735b82e7b43
Laura Sandoval <Laura.Sandoval@lasp.colorado.edu>
2025-07-28 09:00:19 -0600

    added new extension

--- imap_data_access/file_validation.py
+++ imap_data_access/file_validation.py
@@ -793,7 +793,7 @@ class AncillaryFilePath(ImapFilePath):
         "<mission>_<instrument>_<description>_"
         "<start_date>(_<end_date>)_<version>.<extension>"
     )
-    VALID_EXTENSIONS: typing.ClassVar[set[str]] = {"cdf", "csv", "dat", "json", "zip"}
+    VALID_EXTENSIONS: typing.ClassVar[set[str]] = {"cdf", "csv", "dat", "json", "zip", "tsv"}
     _dir_prefix = "imap/ancillary"
 
     class InvalidAncillaryFileError(ImapFilePath.InvalidImapFileError):
